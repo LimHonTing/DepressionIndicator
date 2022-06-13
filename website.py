8cd45708217f5829e677c275b4644696262ca01e
Wong Yan Jian <yanjian1510@gmail.com>
2022-06-13 16:00:22 +0800

    Update website.py

---
+++ website.py
@@ -0,0 +1,390 @@
+# Core Pkgs
+import streamlit as st
+
+# EDA Pkgs
+import pandas as pd
+import numpy as np
+import pickle
+
+import matplotlib
+
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Features names
+feature_names_best = ['Q3A', 'Q5A', 'Q10A', 'Q13A', 'Q16A', 'Q17A', 'Q21A', 'Q24A', 'Q26A', 'Q31A', 'Q34A', 'Q37A',
+                      'Q38A', 'Q42A', 'gender', 'age', 'married']
+
+gender_dict = {"Male": 1, "Female": 2, "Other": 3}
+married_dict = {"Single": 1, "Married": 2, "Divorced": 3}
+feature_dict = {"Did not apply to me at all": 1, "Applied to me to some degree, or some of the time": 2,
+                "Applied to me to a considerable degree, or a good part of the time": 3,
+                "Applied to me very much, or most of the time": 4}
+
+
+def get_value(val, my_dict):
+    for key, value in my_dict.items():
+        if val == key:
+            return value
+
+
+def get_key(val, my_dict):
+    for key, value in my_dict.items():
+        if val == key:
+            return key
+
+
+def get_fvalue(val):
+    feature_dict = {"Did not apply to me at all": 1, "Applied to me to some degree, or some of the time": 2,
+                    "Applied to me to a considerable degree, or a good part of the time": 3,
+                    "Applied to me very much, or most of the time": 4}
+    for key, value in feature_dict.items():
+        if val == key:
+            return value
+
+
+def load_model(model_file):
+    loaded_model = pickle.load(open(model_file, 'rb'))
+    return loaded_model
+
+
+def main():
+    """Depression Indicator"""
+    st.title("Depression Indicator")
+
+    menu = ["Home", "Prediction", "Treatment", "Dataset", "Model Information", "Journey", "About Us"]
+
+    choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Home":
+        mainPage()
+    elif choice == "Treatment":
+        treatmentPage()
+    elif choice == "Prediction":
+        predictionPage()
+
+
+def mainPage():
+    st.subheader("Home")
+    st.image("depression.jpg")
+    st.subheader("What is Depression")
+    st.markdown("""  
+                Depression is a common mental disorder. Globally, it is estimated that 5% of adults suffer from the disorder. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities. It can also disturb sleep and appetite. Tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life.
+                """)
+
+    st.text(" ")
+
+    st.subheader("How Depression is Classified")
+    st.markdown("""
+                It’s common to feel down from time to time, but depression is a separate condition that should be treated with care. Aside from causing a general feeling of sadness, depression is known for causing feelings of hopelessness that don’t seem to go away.
+
+                The term “depression” has become common in mainstream society. But depression is a more nuanced subject than popular usage may suggest. For one, not all cases of depression are the same. There are varying classifications of depression, and each can affect your life in different ways.
+
+                Depression may be classified as:
+                """)
+
+    st.write("• Mild ")
+    st.write("• Moderate ")
+    st.write("• Severe, also called “Major” ")
+
+    st.markdown("""
+                The exact classification is based on many factors. These include the types of symptoms you experience, their severity, and how often they occur. Certain types of depression can also cause a temporary spike in the severity of symptoms.
+
+                Keep reading to learn more about the different classifications of depression and how they may be treated.
+                """)
+
+
+def treatmentPage():
+    st.header('Mild Depression')
+    st.subheader("What does mild depression feel like?")
+    st.markdown("""
+    Mild depression involves more than just feeling blue temporarily. Your symptoms can go on for days and are noticeable enough to interfere with your usual activities.
+    """)
+
+    st.text(" ")
+
+    st.sidebar.markdown('''
+            # Sections
+            - [Mild Depression](#mild-depression)
+            - [Moderate Depression](#moderate-depression)
+            - [Severe Depression](#severe-depression)
+            ''', unsafe_allow_html=False)
+
+    st.subheader("Symptoms of Mild Depression")
+    st.markdown("""
+    •irritability or anger
+
+    •hopelessness
+
+    •feelings of guilt and despair
+
+    •self-loathing
+
+    •a loss of interest in activities you once enjoyed
+
+    •difficulties concentrating at work
+
+    •a lack of motivation
+
+    •a sudden disinterest in socializing
+
+    •aches and pains with seemingly no direct cause
+
+    •daytime sleepiness and fatigue
+
+    •insomnia
+
+    •appetite changes
+
+    •weight changes
+
+    •reckless behavior, such as abuse of alcohol and drugs, or gambling
+
+    If your symptoms persist for most of the day, on an average of four days a week for two years, you would most likely be diagnosed with persistent depressive disorder. This condition is also referred to as dysthymia.
+
+    Though mild depression is noticeable, it’s the most difficult to diagnose. It’s easy to dismiss the symptoms and avoid discussing them with your doctor.
+    """)
+
+    st.text(" ")
+
+    st.subheader("Suggestions to treat Mild Depression")
+    st.markdown("""
+    Despite the challenges in diagnosis, mild depression is the easiest to treat. Certain lifestyle changes can go a long way in boosting serotonin levels in the brain, which can help fight depressive symptoms.
+
+    Helpful lifestyle changes include:
+
+    •exercising daily
+
+    •adhering to a sleep schedule
+
+    •eating a balanced diet rich in fruits and vegetables
+
+    •practicing yoga or meditation
+
+    •doing activities that reduce stress, such as journaling, reading, or listening to music
+
+
+    Other treatments for mild depression include alternative remedies, such as St. John’s Wort and melatonin supplements. However, supplements can interfere with certain medications. Be sure to ask your doctor before taking any supplements for depression.
+
+    A class of antidepressants called selective serotonin reuptake inhibitors (SSRIs) may be used in some cases. However, these tend to be more effective in people with more severe forms of depression. Recurrent depression tends to respond better to lifestyle changes and forms of talk therapy, such as psychotherapy, than medication.
+
+    While medical treatment may not be needed, mild depression won’t necessarily go away on its own. In fact, when left alone, mild depression can progress to more severe forms.
+    """)
+
+    st.text(" ")
+
+    st.header('Moderate Depression')
+    st.subheader("What does moderate depression feel like?")
+    st.markdown("""
+    In terms of symptomatic severity, moderate depression is the next level up from mild cases. Moderate and mild depression share similar symptoms.
+    """)
+
+    st.text(" ")
+
+    st.subheader("Symptoms of Moderate Depression")
+    st.markdown("""
+    •irritability or anger
+
+    •hopelessness
+
+    •feelings of guilt and despair
+
+    •self-loathing
+
+    •a loss of interest in activities you once enjoyed
+
+    •difficulties concentrating at work
+
+    •a lack of motivation
+
+    •a sudden disinterest in socializing
+
+    •aches and pains with seemingly no direct cause
+
+    •daytime sleepiness and fatigue
+
+    •insomnia
+
+    •appetite changes
+
+    •weight changes
+
+    •reckless behavior, such as abuse of alcohol and drugs, or gambling
+
+    •problems with self-esteem
+
+    •reduced productivity
+
+    •feelings of worthlessness
+
+    •increased sensitivities
+
+    •excessive worrying
+
+    The greatest difference is that the symptoms of moderate depression are severe enough to cause problems at home and work. You may also find significant difficulties in your social life.
+    """)
+
+    st.text(" ")
+
+    st.subheader("Suggestions to treat Moderate Depression")
+    st.markdown("""
+    Moderate depression is easier to diagnose than mild cases because the symptoms significantly impact your daily life. The key to a diagnosis, though, is to make sure you talk to your doctor about the symptoms you’re experiencing.
+
+    SSRIs, such as sertraline (Zoloft) or paroxetine (Paxil), may be prescribed. These medications can take up to six weeks to take full effect. Cognitive behavioral therapy (CBT) is also used in some cases of moderate depression.
+    """)
+
+    st.text(" ")
+
+    st.header("Severe Depression")
+    st.subheader("What does severe (major) depression feel like?")
+    st.markdown("""
+    Severe (major) depression is classified as having the symptoms of mild to moderate depression, but the symptoms are severe and noticeable, even to your loved ones.
+
+    Episodes of major depression last an average of six months or longer. Sometimes severe depression can go away after a while, but it can also be recurrent for some people.
+
+    Diagnosis is especially crucial in severe depression, and it may even be time-sensitive.
+    """)
+
+    st.text(" ")
+
+    st.subheader("Symptoms of Severe Depression")
+    st.markdown("""
+    •irritability or anger
+
+    •hopelessness
+
+    •feelings of guilt and despair
+
+    •self-loathing
+
+    •a loss of interest in activities you once enjoyed
+
+    •difficulties concentrating at work
+
+    •a lack of motivation
+
+    •a sudden disinterest in socializing
+
+    •aches and pains with seemingly no direct cause
+
+    •daytime sleepiness and fatigue
+
+    •insomnia
+
+    •appetite changes
+
+    •weight changes
+
+    •reckless behavior, such as abuse of alcohol and drugs, or gambling
+
+    •problems with self-esteem
+
+    •reduced productivity
+
+    •feelings of worthlessness
+
+    •increased sensitivities
+
+    •excessive worrying
+
+    •delusions
+
+    •feelings of stupor
+
+    •hallucinations
+
+    •suicidal thoughts or behaviors
+
+    """)
+
+    st.text(" ")
+
+    st.subheader("Suggestions to treat Severe Depression")
+    st.markdown("""
+    Severe depression requires medical treatment as soon as possible. Your doctor will likely recommend an SSRI and some form of talk therapy.
+
+    If you’re experiencing suicidal thoughts or behaviors, you should seek immediate medical attention. Call your local emergency services or the National Suicide Prevention Lifeline at 800-273-8255 right away.
+    """)
+
+    st.text(" ")
+
+
+def predictionPage():
+    submenu = ["Plot", "Prediction", "Metrics"]
+    activity = st.selectbox("Activity", submenu)
+    if activity == "Plot":
+        # Condition count
+        st.title("Data Vis Plot")
+        df = pd.read_csv("clean_data.csv")
+        st.dataframe(df)
+
+        st.subheader("People Condition of Depression Level")
+        df['Condition'].value_counts().plot(kind='bar')
+        st.pyplot()
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+
+        # Depr.corr
+        st.subheader("Correlation of Depression")
+        fig, ax = plt.subplots()
+        sns.heatmap(df.corr(), ax=ax)
+        st.write(fig)
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+
+        if st.checkbox("Area Chart"):
+            all_columns = df.columns.to_list()
+            feat_choices = st.multiselect("Choose a Feature", all_columns)
+            new_df = df[feat_choices]
+            st.area_chart(new_df)
+
+    elif activity == "Prediction":
+        st.subheader("Predictive Analytics")
+
+        age = st.number_input("Age", 10, 120)
+        gender = st.radio("Gender", tuple(gender_dict.keys()))
+        married = st.radio("Marital Status", tuple(married_dict.keys()))
+        Q3A = st.radio("I couldn't seem to experience any positive feeling at all.",
+                       tuple(feature_dict.keys()))
+        Q5A = st.radio("I just couldn't seem to get going.", tuple(feature_dict.keys()))
+        Q10A = st.radio("I felt that I had nothing to look forward to.", tuple(feature_dict.keys()))
+        Q13A = st.radio("I felt sad and depressed.", tuple(feature_dict.keys()))
+        Q16A = st.radio("I felt that I had lost interest in just about everything.",
+                        tuple(feature_dict.keys()))
+        Q17A = st.radio("I felt I wasn't worth much as a person.", tuple(feature_dict.keys()))
+        Q21A = st.radio("I felt that life wasn't worthwhile.", tuple(feature_dict.keys()))
+        Q24A = st.radio("I couldn't seem to get any enjoyment out of the things I did.",
+                        tuple(feature_dict.keys()))
+        Q26A = st.radio("I felt down-hearted and blue.", tuple(feature_dict.keys()))
+        Q31A = st.radio("I was unable to become enthusiastic about anything.", tuple(feature_dict.keys()))
+        Q34A = st.radio("I felt I was pretty worthless.", tuple(feature_dict.keys()))
+        Q37A = st.radio("I could see nothing in the future to be hopeful about.",
+                        tuple(feature_dict.keys()))
+        Q38A = st.radio("I felt that life was meaningless.", tuple(feature_dict.keys()))
+        Q42A = st.radio("I found it difficult to work up the initiative to do things.",
+                        tuple(feature_dict.keys()))
+        feature_list = [age, get_value(gender, gender_dict), get_value(married, married_dict),
+                        get_fvalue(Q3A), get_fvalue(Q5A), get_fvalue(Q10A), get_fvalue(Q13A),
+                        get_fvalue(Q16A), get_fvalue(Q17A), get_fvalue(Q21A), get_fvalue(Q24A),
+                        get_fvalue(Q26A), get_fvalue(Q31A), get_fvalue(Q34A), get_fvalue(Q37A),
+                        get_fvalue(Q38A), get_fvalue(Q42A)]
+
+        st.write(len(feature_list))
+        pretty_result = {"age": age, "gender": gender, "married": married, "Q3A": Q3A, "Q5A": Q5A,
+                         "Q10A": Q10A, "Q13A": Q13A, "Q16A": Q16A, "Q17A": Q17A, "Q21A": Q21A, "Q24A": Q24A,
+                         "Q24A": Q24A, "Q26A": Q26A, "Q31A": Q31A, "Q34A": Q34A, "Q37A": Q37A, "Q38A": Q38A,
+                         "Q42A": Q42A}
+        st.json(pretty_result)
+        single_sample = np.array(feature_list).reshape(1, -1)
+
+        # ML
+        # model_choice = st.selectbox("Select Model", ["Random Forest", "KNN", "DecisionTree"])
+        # if st.button("Predict"):
+        #     if model_choice == "KNN":
+        #         loaded_model = load_model("KNN.pkl")
+        #         prediction = loaded_model.predict(single_sample)
+        #         pred_prob = loaded_model.predict_proba(single_sample)
+        #         result = loaded_model.score(X_test, Y_test)
+        #         st.write(prediction)
+
+
+if __name__ == '__main__':
+    main()
